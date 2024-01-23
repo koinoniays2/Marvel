155ea055f224b519c5099acbd98f7678627a88cb
koinoniays2 <koinonia_ys@naver.com>
2024-01-23 12:41:01 +0900

    yj-react 20240122에서 이동

---
+++ yj-react/src/routes/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function MainPage() {
+  return (
+    <>
+    <Layout>
+      <div className="py-16">메인페이지</div>
+    </Layout>
+    </>
+  )
+}
